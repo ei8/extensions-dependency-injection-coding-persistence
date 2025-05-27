99d9e44c1897f1df1d9eaa9edbeb381400f8b166
elmerbool <35117496+elmerbool@users.noreply.github.com>
2025-05-27 20:57:28 +1000

    Update TinyIoCContainerExtensions.cs

--- src/main/TinyIoCContainerExtensions.cs
+++ src/main/TinyIoCContainerExtensions.cs
@@ -1,10 +1,7 @@
 ﻿using ei8.Cortex.Coding;
 using ei8.Cortex.Coding.Persistence;
 using ei8.Cortex.Library.Client.Out;
-using Microsoft.Extensions.Options;
 using Nancy.TinyIoc;
-using System.Collections.Generic;
-using System.Net.Http;
 
 namespace ei8.Extensions.DependencyInjection.Coding.Persistence
 {
@@ -17,22 +14,6 @@ namespace ei8.Extensions.DependencyInjection.Coding.Persistence
                     container.Resolve<INeuronQueryClient>(),
                     cortexLibraryOutBaseUrl,
                     queryResultLimit,
-                    appUserId
-                )
-            );
-        }
-
-        public static void AddExternalReferenceRepository(this TinyIoCContainer container, string eventSourcingInBaseUrl, string eventSourcingOutBaseUrl, string cortexLibraryOutBaseUrl, string identityAccessOutBaseUrl, string appUserId)
-        {
-            container.Register<IExternalReferenceRepository>(
-                (tic, npo) => new ExternalReferenceRepository(
-                    container.Resolve<IHttpClientFactory>(),
-                    container.Resolve<INeuronQueryClient>(),
-                    eventSourcingInBaseUrl,
-                    eventSourcingOutBaseUrl,
-                    cortexLibraryOutBaseUrl,
-                    identityAccessOutBaseUrl,
-                    container.Resolve<IOptions<List<ExternalReference>>>(),
                     appUserId
                 )
             );
